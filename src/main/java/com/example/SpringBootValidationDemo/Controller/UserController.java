d9a165725520695119c09026927fd3fc2ecb1a01
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-23 15:12:30 +0300

    First Commit

---
+++ src/main/java/com/example/SpringBootValidationDemo/Controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.SpringBootValidationDemo.Controller;
+
+import com.example.SpringBootValidationDemo.model.User;
+import com.example.SpringBootValidationDemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+
+
+
+    }
+
+
+}
