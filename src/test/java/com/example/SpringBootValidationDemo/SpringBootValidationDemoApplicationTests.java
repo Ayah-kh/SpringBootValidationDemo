73807dfd6ab80980cc3a1e2d65002aa6d01da1cf
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-23 14:40:53 +0300

    First Commit

---
+++ src/test/java/com/example/SpringBootValidationDemo/SpringBootValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootValidationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
