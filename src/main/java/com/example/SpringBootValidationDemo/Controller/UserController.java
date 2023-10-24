94941ddcce93394b636fe35a8b42c3bfaba41d33
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-10-24 14:24:12 +0300

    First Commit

--- src/main/java/com/example/SpringBootValidationDemo/Controller/UserController.java
+++ src/main/java/com/example/SpringBootValidationDemo/Controller/UserController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
@@ -23,12 +20,8 @@ public class UserController {
         User user1 = userService.createUser(user);
         return new ResponseEntity<>(user1, HttpStatus.CREATED);
     }
-
-    public ResponseEntity<User> getUserById(long id){
+ @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable long id){
        return new ResponseEntity<>(userService.getUserById(id),HttpStatus.OK);
     }
-
-
-
-
 }
--- src/main/java/com/example/SpringBootValidationDemo/service/UserService.java
+++ src/main/java/com/example/SpringBootValidationDemo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.SpringBootValidationDemo.service;
 
+import com.example.SpringBootValidationDemo.exeption.ResourceNotFoundException;
 import com.example.SpringBootValidationDemo.model.User;
 import com.example.SpringBootValidationDemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,7 @@ public class UserService {
     }
 
     public User getUserById(long id){
-        User user = userRepository.findById(id).orElseThrow(RuntimeException::new);
+        User user = userRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("User","id",id));
         return user;
     }
 
